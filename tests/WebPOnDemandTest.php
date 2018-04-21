aaa50800394859ea791139720485fa30cee0ecdd
rosell-dk <it@rosell.dk>
2018-04-21 09:59:51 +0200

    added a single test

---
+++ tests/WebPOnDemandTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * WebPConvert - Convert JPEG & PNG to WebP with PHP
+ *
+ * @link https://github.com/rosell-dk/webp-convert
+ * @license MIT
+ */
+
+namespace WebPOnDemand\Tests;
+
+use WebPOnDemand\WebPOnDemand;
+use WebPConvertAndServe\WebPConvertAndServe;
+use PHPUnit\Framework\TestCase;
+
+class WebPOnDemandTest extends TestCase
+{
+    public function testConvertWithNoConverters()
+    {
+        $this->assertEquals($_GET, []);
+
+
+        $_GET['source'] = 'test.jpg';
+        $_GET['converters'] = 'nonexistant';
+        $_GET['fail'] = '404';
+        $_GET['critical-fail'] = '404';
+
+        ob_start();
+        $result = WebPOnDemand::serve(__DIR__);
+        $output = ob_get_contents();
+
+        $this->assertEquals($result, WebPConvertAndServe::$HTTP_404);
+    }
+}
